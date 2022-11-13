bfbc65aa0f97a6bc1192ebfeb8addce8a6501804
Usama Ahmed Siddiquie <usiddique@cognitivehealthintl.com>
2022-11-13 22:58:24 +0500

    Initial commit

---
+++ Sources/CallsSDK.xcframework/ios-arm64/CallsSDK.framework/Headers/CallsSDK.h
@@ -0,0 +1,18 @@
+//
+//  CallsSDK.h
+//  CallsSDK
+//
+//  Created by CHI on 12/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CallsSDK.
+FOUNDATION_EXPORT double CallsSDKVersionNumber;
+
+//! Project version string for CallsSDK.
+FOUNDATION_EXPORT const unsigned char CallsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CallsSDK/PublicHeader.h>
+
+
--- Sources/CallsSDK/CallsSDK.swift
+++
@@ -1,6 +0,0 @@
-public struct CallsSDK {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
